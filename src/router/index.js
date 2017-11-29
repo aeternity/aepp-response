aeternity/aepp-wall

Add initial frontend

src/router/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import RecordList from '../components/RecordList';
+import RecordDetail from '../components/RecordDetail';
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   routes: [
+    { path: '/record/:id', name: 'record', component: RecordDetail, props: true },
+    { path: '/:sort?', name: 'record-list', component: RecordList, props: true },
   ],
 });

aeternity/aepp-response

src/router/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import QuestionList from '../components/QuestionList';
+import QuestionDetail from '../components/QuestionDetail';
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   routes: [
+    { path: '/question/:id', name: 'question', component: QuestionDetail, props: true },
+    { path: '/:sort?', name: 'question-list', component: QuestionList, props: true },
   ],
 });