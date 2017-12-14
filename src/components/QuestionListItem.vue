d23750c0acc0dd71e8c8c5ab9f14247dc7e8ad80
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2017-12-14 18:37:54 +1000

    QuestionListItem: Put question in header

--- src/components/QuestionListItem.vue
+++ src/components/QuestionListItem.vue
@@ -7,11 +7,8 @@
       <ae-category v-if="question.tweetId">answered</ae-category>
       <img :src="`https://twitter.com/${question.twitter}/profile_image?size=original`" />
       <div class="content">
-        <h2>@{{question.twitter}}</h2>
-        <text-muted>
-          {{question.title}}
-          <template v-if="question.body"> – {{question.body}}</template>
-        </text-muted>
+        <h2>@{{question.twitter}}, {{question.title}}</h2>
+        <text-muted v-if="question.body">{{question.body}}</text-muted>
         <question-statistic :question="question" />
       </div>
       <div class="arrow">
